296de809cb308c44f1aadd530242e09d3c85063a
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-02-02 17:19:05 +0700

    ok

---
+++ week3/ex1.dart
@@ -0,0 +1,78 @@
+import 'dart:ffi';
+
+class SinhVien {
+  var maSinhVien;
+  var hoTen;
+  var gioiTinh;
+
+  void hienThiThongTin() {
+    print("Ma sinh vien: $maSinhVien ");
+    print("Ho va Ten: $hoTen");
+    print("Ma sinh vien: ${gioiTinh ? "Nam" : "Nu"} ");
+  }
+
+  SinhVien() {
+    maSinhVien = 21010598;
+    hoTen = "Nguyen Phi Hung";
+    gioiTinh = true;
+  }
+
+  // SinhVien(SinhVien s) {
+  //   this.maSinhVien = s.maSinhVien;
+  //   this.hoTen = s.hoTen;
+  //   this.gioiTinh = s.gioiTinh
+  // }
+  SinhVien.khoiTaoGiaTri(int maSinhVien, String hoTen, bool gioiTinh) {
+    this.maSinhVien = maSinhVien;
+    this.hoTen = hoTen;
+    this.gioiTinh = gioiTinh;
+  }
+  set ident(String maSinhVien) {
+    this.maSinhVien = maSinhVien;
+  }
+
+  set name(String hoTen) {
+    this.hoTen = hoTen;
+  }
+
+  void set sex(bool gioiTinh) {
+    this.gioiTinh = gioiTinh;
+  }
+
+  int get identify {
+    return maSinhVien;
+  }
+
+  String get name {
+    return hoTen;
+  }
+
+  bool get sex {
+    return gioiTinh;
+  }
+}
+
+class SinhVienDaiCuong extends SinhVien {
+  var diemDaiCuong;
+  @override
+  void hienThiThongTin() {
+    print("Ma sinh vien: $maSinhVien ");
+    print("Ho va Ten: $hoTen");
+    print("Ma sinh vien: ${gioiTinh ? "Nam" : "Nu"} ");
+  }
+}
+
+void main(List<String> args) {
+  var s = new SinhVien();
+  var sv = new SinhVien.khoiTaoGiaTri(210100598, "Nguyen Phi Hung", false);
+  var svv = new SinhVienDaiCuong();
+  svv.name = "Hungdz";
+  svv.sex = true;
+  // sv.maSinhVien = 21010598;
+  // sv.hoTen = "Nguyen Phi Hung";
+  // sv.gioiTinh = true;
+  s.hienThiThongTin();
+  print("------------------");
+
+  svv.hienThiThongTin();
+}
