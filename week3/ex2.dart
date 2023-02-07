a41a344e5e5f9b223955a7509cfd95f392e73510
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-02-07 08:24:40 +0700

    xong cau6

--- week3/ex2.dart
+++ week3/ex2.dart
@@ -1,4 +1,42 @@
 abstract class Hinh {
-  void getChuVi();
-  void getDienTich();
+  double getChuVi();
+  double getDienTich();
+  void hienThiKichThuoc() {
+    print("Hinh co Chu Vi la: ${getChuVi()}");
+    print("$getDienTich()");
+  }
+}
+
+class HinhTron extends Hinh {
+  var r;
+  HinhTron(double r) {
+    this.r = r;
+  }
+  double getChuVi() {
+    return 2 * 3.14 * r;
+  }
+
+  double getDienTich() {
+    return 3.14 * r * r;
+  }
+}
+
+class HinhChuNhat extends Hinh {
+  var chieuDai;
+  var chieuRong;
+  HinhChuNhat(double chieuDai, double chieuRong) {}
+  @override
+  double getChuVi() {
+    return 2.0 * (chieuDai + chieuRong);
+  }
+
+  @override
+  double getDienTich() {
+    return chieuDai * chieuRong;
+  }
+}
+
+void main(List<String> args) {
+  Hinh ht1 = HinhTron(5.5);
+  Hinh ht2 = HinhChuNhat(3.3, 4.4);
 }
